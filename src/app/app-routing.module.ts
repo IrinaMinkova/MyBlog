8a8a54673b5f6502fa6a11dda841ab5e4f7d796b
Irina Minkova <irina.ext.minkova@swedbank.se>
2020-01-21 14:27:01 +0100

    Create main layout, routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {MainLayoutComponent} from './shared/components/main-layout/main-layout.component';
+import {HomePageComponent} from './home-page/home-page.component';
+import {PostPageComponent} from './post-page/post-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: MainLayoutComponent, children: [
+      {path: '', redirectTo: '/', pathMatch:'full' }, //redirect to the empty page, to make Angular understand when we go to the '' page.
+        //If the path is empty then we upload MainLayoutComponent, / - is the path to HomePageComponent, 'full' means that the path should be completly the same
+      {path: '', component: HomePageComponent },
+      {path: 'post/:id', component: PostPageComponent}
+    ]
+  }
+  ]
+;
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
