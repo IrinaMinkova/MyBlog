69a171c98bbd8d4b8bb3133d47457dbeb50d47e8
Irina Minkova <irina.ext.minkova@swedbank.se>
2020-01-23 10:48:41 +0100

    Add admin module, createsadmin layout, design of the main page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {Routes, RouterModule, PreloadAllModules} from '@angular/router';
 import {MainLayoutComponent} from './shared/components/main-layout/main-layout.component';
 import {HomePageComponent} from './home-page/home-page.component';
 import {PostPageComponent} from './post-page/post-page.component';
@@ -8,17 +8,23 @@ import {PostPageComponent} from './post-page/post-page.component';
 const routes: Routes = [
   {
     path: '', component: MainLayoutComponent, children: [
-      {path: '', redirectTo: '/', pathMatch:'full' }, //redirect to the empty page, to make Angular understand when we go to the '' page.
-        //If the path is empty then we upload MainLayoutComponent, / - is the path to HomePageComponent, 'full' means that the path should be completly the same
+      {path: '', redirectTo: '/', pathMatch: 'full' }, // redirect to the empty page, to make Angular understand when we go to the '' page.
+      // tslint:disable-next-line:max-line-length
+        // If the path is empty then we upload MainLayoutComponent, / - is the path to HomePageComponent, 'full' means that the path should be completly the same
       {path: '', component: HomePageComponent },
       {path: 'post/:id', component: PostPageComponent}
     ]
+  },
+  {
+    path: 'admin', loadChildren: './admin/admin.module#AdminModule'
   }
   ]
 ;
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    preloadingStrategy: PreloadAllModules
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
