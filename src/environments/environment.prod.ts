f5a532ba46f7775d17f90f9b976ccb929f60e024
Irina Minkova <irina.ext.minkova@swedbank.se>
2020-01-24 09:04:50 +0100

    Set up Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
-export const environment = {
-  production: true
+import {Environment} from './interface';
+
+export const environment: Environment = {
+  production: true,
+  apiKey: 'AIzaSyD9O9eu1xMVVqJNcb1LEZLdBwbWhXD07ms'
 };
