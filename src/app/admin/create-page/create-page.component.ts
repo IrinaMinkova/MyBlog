0c86d5eb49459622955e9720a465b77613d52723
Irina Minkova <irina.ext.minkova@swedbank.se>
2020-01-29 14:00:30 +0100

    Complete the function: show the list of posts

--- src/app/admin/create-page/create-page.component.ts
+++ src/app/admin/create-page/create-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {Post} from '../../shared/interfaces';
+import {PostsService} from '../../shared/posts.service';
 
 @Component({
   selector: 'app-create-page',
@@ -11,7 +12,7 @@ export class CreatePageComponent implements OnInit {
 
   form: FormGroup;
 
-  constructor() { }
+  constructor(private postsService: PostsService) { }
 
   ngOnInit() {
     this.form = new FormGroup({
@@ -26,11 +27,17 @@ export class CreatePageComponent implements OnInit {
       return;
     }
     const post: Post = {
-  title: this.form.value.title,
+      title: this.form.value.title,
       author: this.form.value.author,
       text: this.form.value.text,
       date: new Date(),
     };
+
+    this.postsService.create(post).subscribe(() => {
+      this.form.reset();
+    })
+
+    console.log(post);
   }
 
 }
