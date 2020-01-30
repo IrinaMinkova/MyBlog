d203942a23a58bf50da38e2f78386e12d85d6565
Irina Minkova <irina.ext.minkova@swedbank.se>
2020-01-30 16:25:14 +0100

    Create message alert plugin

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -12,6 +12,8 @@ import { EditPageComponent } from './edit-page/edit-page.component';
 import {SharedModule} from '../shared/shared.module';
 import {AuthGuard} from './shared/services/auth.guard';
 import {SearchPipe} from './shared/search.pipe';
+import { AlertComponent } from './shared/components/alert/alert.component';
+import {AlertService} from './shared/services/alert.service';
 
 @NgModule ({
     imports: [
@@ -40,9 +42,10 @@ import {SearchPipe} from './shared/search.pipe';
         DashboardPageComponent,
         CreatePageComponent,
         EditPageComponent,
-        SearchPipe
+        SearchPipe,
+        AlertComponent
     ],
-    providers: [AuthGuard]
+    providers: [AuthGuard, AlertService]
 })
 
 export class AdminModule {
